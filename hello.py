5c763cbdb43247f9236447e983ca99270cd4a28c
classuser158 <classuser158@local.iful.edu.ua>
2025-11-21 12:38:37 +0200

    Something new

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
+
+
 a = 5           # робимо змінну
 if a == 5:      # задаємо умову
-    print("Hello!") 
+    print("Hello!")
+b = a + 3
+if b == 5:
+    print("Hello!")
+else:
+    print("undefined")
